d557840a9c1f3e6ece0ea093a9d447045f94fb0b
금가경 <gageum0@gmail.com>
2025-12-16 19:24:23 +0900

    NewsAdItem 컴포넌트 추가

---
+++ Rewp/Components/NewsAdItem.swift
@@ -0,0 +1,65 @@
+import UIKit
+import PinLayout
+import Then
+
+final class NewsAdItem: UIView {
+    private let containerView = UIView().then {
+        $0.backgroundColor = ColorSystem.gray0
+    }
+
+    private let titleLabel = UILabel().then {
+        $0.textColor = ColorSystem.gray75
+    }
+
+    private let descriptionLabel = UILabel().then {
+        $0.textColor = ColorSystem.gray45
+    }
+
+    init(title: String, description: String) {
+        super.init(frame: .zero)
+        titleLabel.typography(FontSystem.Pretendard.body2, text: title)
+        descriptionLabel.typography(FontSystem.Pretendard.caption2, text: description)
+        setupUI()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupUI() {
+        addSubview(containerView)
+        containerView.addSubview(titleLabel)
+        containerView.addSubview(descriptionLabel)
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        containerView.pin.all()
+
+        titleLabel.pin
+            .top(20)
+            .left(20)
+            .right(20)
+            .sizeToFit(.width)
+
+        descriptionLabel.pin
+            .below(of: titleLabel)
+            .marginTop(8)
+            .left(20)
+            .right(20)
+            .sizeToFit(.width)
+    }
+
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: UIView.noIntrinsicMetric, height: 66)
+    }
+}
+
+@available(iOS 17.0, *)
+#Preview {
+    NewsAdItem(
+        title: "배너 광고 타이틀 문구",
+        description: "배너 광고 설명 서브타이틀 문구"
+    )
+}
