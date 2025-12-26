8416f0bef5b39cde2b170bc06f345ed3c2a261cf
금가경 <gageum0@gmail.com>
2025-12-26 14:43:58 +0900

    네트워크 서비스 추가

---
+++ Rewp/Core/Network/NetworkService.swift
@@ -0,0 +1,27 @@
+//  NetworkService.swift
+//  Rewp
+//
+//  Created by 금가경 on 12/26/25.
+
+import Foundation
+import Moya
+import RxSwift
+
+protocol NetworkServiceProtocol {
+    func request<T: Decodable>(_ target: TargetType) -> Single<T>
+}
+
+final class NetworkService: NetworkServiceProtocol {
+    private let provider: MoyaProvider<MultiTarget>
+
+    init(provider: MoyaProvider<MultiTarget> = MoyaProvider<MultiTarget>()) {
+        self.provider = provider
+    }
+
+    func request<T: Decodable>(_ target: TargetType) -> Single<T> {
+        return provider.rx
+            .request(MultiTarget(target))
+            .filterSuccessfulStatusCodes()
+            .map(T.self)
+    }
+}
